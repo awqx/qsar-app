d676ec4f34a49a81c6c93042cd124cb8873e7ec1
awqx <alisonwxin@gmail.com>
2019-01-10 22:33:04 -0500

    Setting up a basic layout

--- app.R
+++ app.R
@@ -5,16 +5,23 @@ source("./functions.R")
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Cyclodextrin Affinity Prediction"),
    
+   # Start with a file input for .csv of descriptors
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+         fileInput("padel",
+                   "PaDEL-Descriptor Output",
+                   multiple = T, 
+                   accept = c(
+                     "text/csv",
+                     "text/comma-separated-values,text/plain",
+                     ".csv")), 
+         br(), 
+         checkboxGroupInput("cd", "Cyclodextrin Type", 
+                            choices = list("Alpha" = "a", "Beta" = "b", "Gamma" = "c"))
+         
       ),
       
       # Show a plot of the generated distribution
@@ -26,7 +33,20 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
+  
+  # Creating a reactive dataframe of ensemble prediction results
+  # that responds to loading a file
+  pred <- reactive({
+    # Checks if a file has been loaded
+    if(is.null(input$padel))
+      return(NULL)
+    
+    # The local path for the file is stored in datapath
+    raw <- read.csv(input$padel$datapath, header = T)
+    
+    
+  })
+  
    output$distPlot <- renderPlot({
       # generate bins based on input$bins from ui.R
       x    <- faithful[, 2] 
